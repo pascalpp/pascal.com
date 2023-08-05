d414ae13e594d850ed16f74d11ac0b1807bdaea2
Pascal <pascal@pascal.com>
2023-08-05 01:02:16 -0400

    fixed md/svg typescript warnings

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,4 +9,15 @@ declare global {
 	}
 }
 
-export {};
+declare module '*.md' {
+	import type { SvelteComponent } from 'svelte';
+
+	export default class Comp extends SvelteComponent {}
+	export const metadata: Record<string, string>;
+}
+
+declare module '*.svg?component' {
+	import type { SvelteComponent } from 'svelte';
+
+	export default class Comp extends SvelteComponent {}
+}
