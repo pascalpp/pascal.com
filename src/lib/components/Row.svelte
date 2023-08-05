f80c8b77e4cf529eebaeaea5f0784c42af7973a2
Pascal <pascal@pascal.com>
2023-08-04 20:15:56 -0400

    use class:foo for row classes

--- src/lib/components/Row.svelte
+++ src/lib/components/Row.svelte
@@ -1,59 +1,61 @@
 <script lang="ts">
-  import classnames from 'classnames';
-
-  export let center = false;
-  export let right = false;
-  export let spread = false;
-  export let stretch = false;
-  export let top = false;
-  export let bottom = false;
-  export let wrap = false;
-  export let gap = '1em';
-
-  const className = classnames('row', {
-    center,
-    right,
-    spread,
-    stretch,
-    top,
-    bottom,
-    wrap,
-  });
+	export let center = false;
+	export let right = false;
+	export let spread = false;
+	export let stretch = false;
+	export let top = false;
+	export let bottom = false;
+	export let wrap = false;
+	export let reverse = false;
+	export let gap = '1em';
 </script>
 
-<section class={className} style="gap: {gap}">
-  <slot />
+<section
+	class="row"
+	class:center
+	class:right
+	class:spread
+	class:stretch
+	class:top
+	class:bottom
+	class:wrap
+	class:reverse
+	style="gap: {gap}">
+	<slot />
 </section>
 
 <style lang="less">
-  .row {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    align-items: center;
+	.row {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: flex-start;
+		align-items: center;
 
-    &.center {
-      justify-content: center;
-    }
-    &.right {
-      justify-content: flex-end;
-    }
-    &.spread {
-      justify-content: space-between;
-    }
+		&.center {
+			justify-content: center;
+		}
+		&.right {
+			justify-content: flex-end;
+		}
+		&.spread {
+			justify-content: space-between;
+		}
 
-    &.top {
-      align-items: flex-start;
-    }
-    &.bottom {
-      align-items: flex-end;
-    }
-    &.stretch {
-      align-items: stretch;
-    }
-    &.wrap {
-      flex-wrap: wrap;
-    }
-  }
+		&.top {
+			align-items: flex-start;
+		}
+		&.bottom {
+			align-items: flex-end;
+		}
+		&.stretch {
+			align-items: stretch;
+		}
+		&.wrap {
+			flex-wrap: wrap;
+		}
+		&.reverse {
+			flex-direction: row-reverse;
+		}
+	}
 </style>
