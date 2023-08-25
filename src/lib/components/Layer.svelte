9c1e7cd8fb71f60ee80a168eb40a6e28fca6c7ff
Pascal <pascal@pascal.com>
2023-08-24 22:20:22 -0400

    layer just displays contents until cropped

--- src/lib/components/Layer.svelte
+++ src/lib/components/Layer.svelte
@@ -1,64 +1,70 @@
 <script lang="ts" context="module">
-  export interface Layer {
-    cropped: boolean;
-    crop: () => void;
-    uncrop: () => void;
-  }
+	export interface Layer {
+		cropped: boolean;
+		crop: () => void;
+		uncrop: () => void;
+	}
 </script>
 
 <script lang="ts">
-  import { setContext } from 'svelte';
-  import { writable } from 'svelte/store';
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
 
-  let top = 0;
-  let cropped = false;
+	let top = 0;
+	let cropped = false;
 
-  export function crop() {
-    if (cropped) return;
-    top = window.scrollY;
-    cropped = true;
-  }
+	export function crop() {
+		if (cropped) return;
+		top = window.scrollY;
+		cropped = true;
+	}
 
-  export function uncrop() {
-    if (!cropped) return;
-    cropped = false;
-    requestAnimationFrame(() => window.scrollTo({ top }));
-  }
+	export function uncrop() {
+		if (!cropped) return;
+		cropped = false;
+		requestAnimationFrame(() => window.scrollTo({ top }));
+	}
 
-  const layer = writable({ cropped, crop, uncrop });
-  setContext('layer', layer);
+	const layer = writable({ cropped, crop, uncrop });
+	setContext('layer', layer);
 
-  $: layer.update((previous) => ({ ...previous, cropped }));
+	$: layer.update((previous) => ({ ...previous, cropped }));
 </script>
 
 <div class="layer" class:cropped>
-  <div class="layer-content" style="top: -{top}px">
-    <slot {cropped} {crop} {uncrop} />
-  </div>
+	<div class="layer-content" style="top: -{top}px">
+		<slot {cropped} {crop} {uncrop} />
+	</div>
 </div>
 
 <style lang="less">
-  .layer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    box-sizing: border-box;
+	.layer {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		box-sizing: border-box;
+		display: contents;
+		> .layer-content {
+			display: contents;
+		}
 
-    &.cropped {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      overflow: hidden;
+		&.cropped {
+			display: block;
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100vw;
+			height: 100vh;
+			overflow: hidden;
 
-      > .layer-content {
-        box-sizing: border-box;
-        position: absolute;
-        left: 0;
-        width: 100vw;
-      }
-    }
-  }
+			> .layer-content {
+				display: block;
+				box-sizing: border-box;
+				position: absolute;
+				left: 0;
+				width: 100vw;
+			}
+		}
+	}
 </style>
