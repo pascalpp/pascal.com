199e49147c0c5ad922e28adfddd34106c4340f8b
Pascal <pascal@pascal.com>
2023-08-25 03:38:57 -0400

    add transition to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,35 @@
 
 	import Layer from '$lib/components/Layer.svelte';
 	import Toaster from '$lib/components/Toaster.svelte';
+	import { fade } from 'svelte/transition';
+
+	export let data;
+
+	console.log({ data });
 </script>
 
 <Layer>
 	<Toaster>
-		<slot />
+		<div class="transition-container">
+			{#key data.currentRoute.split('/')[1]}
+				<main in:fade={{ duration: 250, delay: 150 }} out:fade={{ duration: 250 }}>
+					<slot />
+				</main>
+			{/key}
+		</div>
 	</Toaster>
 </Layer>
+
+<style lang="less">
+	.transition-container {
+		width: 100%;
+		display: grid;
+		grid-template-rows: 1fr;
+		grid-template-columns: 1fr;
+	}
+
+	.transition-container > * {
+		grid-row: 1;
+		grid-column: 1;
+	}
+</style>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,10 @@
 export const prerender = true;
 export const trailingSlash = 'always';
+
+export const load = ({ url }) => {
+	const currentRoute = url.pathname;
+
+	return {
+		currentRoute,
+	};
+};
