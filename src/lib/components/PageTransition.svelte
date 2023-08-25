9471a90859c1d2d981ceff6f01b3321b4b40c2d6
Pascal <pascal@pascal.com>
2023-08-24 22:21:00 -0400

    Update PageTransition.svelte

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -1,31 +1,32 @@
+<!-- https://dev.to/evanwinter/page-transitions-with-svelte-kit-35o6 -->
 <!-- PageTransition.svelte -->
 <script lang="ts">
-  import { fly } from 'svelte/transition';
-  export let url = '';
-  export let distance: number;
+	import { fly } from 'svelte/transition';
+	export let url = '';
+	export let distance: number;
 
-  const duration = 250;
-  const opacity = 0;
+	const duration = 250;
+	const opacity = 0;
 </script>
 
 <div class="transition-container">
-  {#key url}
-    <div in:fly={{ x: distance, duration, opacity }} out:fly={{ x: -distance, duration, opacity }}>
-      <slot />
-    </div>
-  {/key}
+	{#key url}
+		<div in:fly={{ x: distance, duration, opacity }} out:fly={{ x: -distance, duration, opacity }}>
+			<slot />
+		</div>
+	{/key}
 </div>
 
 <style>
-  .transition-container {
-    width: 100%;
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-template-columns: 1fr;
-  }
+	.transition-container {
+		width: 100%;
+		display: grid;
+		grid-template-rows: 1fr;
+		grid-template-columns: 1fr;
+	}
 
-  .transition-container > * {
-    grid-row: 1;
-    grid-column: 1;
-  }
+	.transition-container > * {
+		grid-row: 1;
+		grid-column: 1;
+	}
 </style>
