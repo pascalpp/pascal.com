0839f17bc54c64126b57cb4249e38f01325903c0
Pascal <pascal@pascal.com>
2023-09-01 01:04:44 -0400

    provide pathname and search

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,9 +2,10 @@ export const prerender = true;
 export const trailingSlash = 'always';
 
 export const load = ({ url }) => {
-	const currentRoute = url.pathname;
+	const { pathname, search } = url;
 
 	return {
-		currentRoute,
+		pathname,
+		search,
 	};
 };
