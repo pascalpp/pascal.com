fcf53e3163901ba55ccd1c61c2d27f783f92b6fa
Pascal <pascal@pascal.com>
2023-09-01 01:14:45 -0400

    look for tag query string on diary index route

--- src/routes/diary/+page.ts
+++ src/routes/diary/+page.ts
@@ -1,11 +1,22 @@
 // src/routes/diary/+page.js
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch(`/api/posts`);
+export const load: PageLoad = async ({ fetch, url }) => {
+	const tag = url.searchParams.get('tag');
+
+	let apiUrl;
+
+	if (tag) {
+		apiUrl = `/api/posts?tag=${tag}`;
+	} else {
+		apiUrl = `/api/posts`;
+	}
+
+	const response = await fetch(apiUrl);
 	const posts = await response.json();
 
 	return {
 		posts,
+		tag,
 	};
 };
