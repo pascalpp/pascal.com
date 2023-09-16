dab83252cc666ec7bf79273442d9ea8a1e473ccb
Pascal <pascal@pascal.com>
2023-09-16 01:38:34 -0400

    Create PageConnections.svelte

---
+++ src/routes/fleuve/PageConnections.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageId } from './Page.model';
+	import PageCard from './PageCard.svelte';
+
+	export let connections: PageId[];
+	export let onAddPage: (event: KeyboardEvent) => void;
+</script>
+
+<div class="connections">
+	{#each connections as connectionId}
+		<PageCard pageId={connectionId} />
+	{/each}
+	<div autofocus={true} contenteditable="true" class="add-connection" on:keydown={onAddPage} />
+</div>
