cfa5f878d45ab5b22fd000a7e608816cccdfc716
Pascal <pascal@pascal.com>
2023-09-17 13:32:29 -0400

    got markdown support in description

--- src/routes/fleuve/PageDescription.svelte
+++ src/routes/fleuve/PageDescription.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	import type { Page } from './Page.model';
 	import { updatePage } from './store';
+	import { marked } from 'marked';
 
 	export let page: Page;
 	export let tabindex: number;
+
+	let editing = false;
+
+	function onFocus(event: FocusEvent) {
+		const target = event.target as HTMLElement;
+		editing = true;
+		requestAnimationFrame(() => {
+			const editor = target?.querySelector('.editor') as HTMLElement;
+			editor?.focus();
+		});
+	}
 
 	function onBlur(event: FocusEvent) {
 		const target = event.target as HTMLElement;
 		const description = target.innerText.trim();
 		updatePage({ ...page, description });
+		editing = false;
 	}
 
 	function onKeyDown(event: KeyboardEvent) {
@@ -36,12 +49,20 @@
 	}
 </script>
 
-<p
-	class="description"
-	contenteditable={page.active}
-	tabindex={page.active ? tabindex : -1}
-	on:blur={onBlur}
-	on:keydown={onKeyDown}
->
-	{page.description || ''}
-</p>
+<div class="description" tabindex={page.active && !editing ? tabindex : -1} on:focus={onFocus}>
+	{#if editing}
+		<div
+			class="editor"
+			tabindex={page.active ? tabindex : -1}
+			contenteditable={page.active}
+			on:blur={onBlur}
+			on:keydown={onKeyDown}
+		>
+			{page.description || ''}
+		</div>
+	{:else}
+		<div class="content">
+			{@html marked.parse(page.description || '')}
+		</div>
+	{/if}
+</div>
