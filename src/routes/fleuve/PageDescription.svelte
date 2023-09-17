100962795a1c2ebb3fb0d11a44a9b2c4a0463af3
Pascal <pascal@pascal.com>
2023-09-16 22:30:52 -0400

    left/right key support in description

--- src/routes/fleuve/PageDescription.svelte
+++ src/routes/fleuve/PageDescription.svelte
@@ -5,15 +5,33 @@
 	export let page: Page;
 	export let tabindex: number;
 
-	function onBlurDescription(event: FocusEvent) {
+	function onBlur(event: FocusEvent) {
 		const target = event.target as HTMLElement;
-		const description = target.innerText;
+		const description = target.innerText.trim();
 		updatePage({ ...page, description });
 	}
 
-	function onKeyDownDescription(event: KeyboardEvent) {
+	function onKeyDown(event: KeyboardEvent) {
+		const target = event.target as HTMLElement;
+		const description = target.innerText.trim();
 		if (event.key !== 'Tab') {
 			event.stopPropagation();
+		}
+
+		if (event.key === 'ArrowLeft' && !description) {
+			const parentCard = target.closest('.connections')?.closest('.page')?.querySelector('.page-card') as HTMLElement;
+			target.innerText = description;
+			parentCard?.focus();
+			parentCard?.click();
+		}
+
+		if (event.key === 'ArrowRight' && !description) {
+			target.innerText = description;
+			const firstChild = target
+				.closest('.page')
+				?.querySelector('.connections')
+				?.querySelector('.page-card, .add-connection') as HTMLElement;
+			firstChild?.focus();
 		}
 	}
 </script>
@@ -22,7 +40,8 @@
 	class="description"
 	contenteditable={page.active}
 	tabindex={page.active ? tabindex : -1}
-	on:blur={onBlurDescription}
-	on:keydown={onKeyDownDescription}>
+	on:blur={onBlur}
+	on:keydown={onKeyDown}
+>
 	{page.description || ''}
 </p>
