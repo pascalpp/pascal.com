e3e61419b4196412dad3b78a82cab55ecb8a81fd
Pascal <pascal@pascal.com>
2023-09-17 23:53:03 -0400

    move connections styles

--- src/routes/fleuve/PageConnections.svelte
+++ src/routes/fleuve/PageConnections.svelte
@@ -7,9 +7,72 @@
 	export let tabindex: number;
 </script>
 
-<div class="connections">
+<div class="connections" class:active={page.active}>
 	{#each page.connections as connectionId (connectionId)}
 		<PageCard pageId={connectionId} {tabindex} />
 	{/each}
 	<AddPageCard {page} {tabindex} />
 </div>
+
+<style lang="less">
+	.connections {
+		transform: translateX(-2px);
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 1em;
+
+		opacity: var(--child-opacity, 0.5);
+		&.active {
+			opacity: 1;
+		}
+
+		// set placeholder text for the add page card
+		--add-placeholder: 'Add page';
+		&:has(.page.active) {
+			--add-placeholder: 'Add sibilng';
+		}
+
+		// show left border on connections block if there are multiple items in it
+		&.active:has(:first-child:not(:last-child)),
+		&:has(.page + .page) {
+			margin-left: 24px;
+			border-left: 1px solid black;
+			&::before {
+				position: absolute;
+				display: block;
+				content: '';
+				height: 1px;
+				width: 24px;
+				background-color: black;
+				top: 50%;
+				right: 100%;
+			}
+		}
+
+		// show connection line on left side of each child
+		:global(> *:not(.page.active)) {
+			margin-left: 24px;
+			&::before {
+				position: absolute;
+				display: block;
+				content: '';
+				height: 1px;
+				width: 24px;
+				background-color: black;
+				top: 50%;
+				right: 100%;
+			}
+		}
+
+		//  hide siblings idea
+		// .connections:has(.page.active) {
+		// 	> .page:not(.active) {
+		// 		display: none;
+		// 	}
+		// 	> .add-connection {
+		// 		display: none;
+		// 	}
+		// }
+	}
+</style>
