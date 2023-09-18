0406876dc589fe027a0cd1bfad32ecff0bf3c49e
Pascal <pascal@pascal.com>
2023-09-17 23:31:10 -0400

    add settings store

---
+++ src/routes/fleuve/settings.store.ts
@@ -0,0 +1,42 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+import { metadata } from './changelog.md';
+
+const storageKey = 'settings';
+
+export type AspectRatio = 'landscape' | 'portrait' | 'square';
+
+export type Settings = {
+	version: string;
+	childOpacity: number;
+	activePageScale: number;
+	aspectRatioType: AspectRatio;
+};
+
+export const settings = writable<Settings>(getStoredState() || getDefaultState());
+
+if (browser) {
+	settings.subscribe((data) => {
+		window?.localStorage.setItem(storageKey, JSON.stringify(data));
+	});
+}
+
+function getStoredState(): Settings | undefined {
+	if (browser) {
+		try {
+			const state = window?.localStorage.getItem(storageKey);
+			return state && JSON.parse(state);
+		} catch {
+			// don't care
+		}
+	}
+}
+
+function getDefaultState(): Settings {
+	return {
+		version: metadata.latest,
+		childOpacity: 0.5,
+		activePageScale: 0.2,
+		aspectRatioType: 'landscape' as AspectRatio,
+	};
+}
