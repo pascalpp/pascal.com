381deb00b9a9c40a29223bb46f0e4c96289d7ffa
Pascal <pascal@pascal.com>
2023-09-19 12:14:29 -0400

    Create ResetButton.svelte

---
+++ src/routes/fleuve/ResetButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { reset } from './pages.store';
+
+	function onClickReset() {
+		if (confirm('Are you sure you want to start over?')) {
+			reset();
+		}
+	}
+</script>
+
+<button class="reset-button" on:click={onClickReset}>Reset data…</button>
+
+<style lang="less"></style>
