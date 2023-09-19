9d53126975da0b578b0a78538a81891e668dba63
Pascal <pascal@pascal.com>
2023-09-19 11:59:52 -0400

    improved api and appearance of Toolbar

--- src/routes/fleuve/Toolbar.svelte
+++ src/routes/fleuve/Toolbar.svelte
@@ -3,31 +3,38 @@
 	export let bottom = false;
 	export let left = false;
 	export let right = false;
-	export let column = false;
-	export let row = false;
 	export let show = false;
+
+	let toolbar: HTMLDivElement;
 
 	function toggle() {
 		show = !show;
 		if (show) {
-			document.body.addEventListener('click', toggle);
+			document.body.addEventListener('click', onClickOutside);
 		} else {
-			document.body.removeEventListener('click', toggle);
+			document.body.removeEventListener('click', onClickOutside);
+		}
+	}
+
+	function onClickOutside(event: MouseEvent) {
+		const target = event.target as HTMLElement;
+		if (!toolbar.contains(target)) {
+			show = false;
 		}
 	}
 
 	function onClick(event: MouseEvent) {
-		event.stopPropagation();
+		if (!show) event.stopPropagation();
 	}
 </script>
 
-<div class="toolbar" class:top class:bottom class:left class:right class:show on:click={onClick}>
-	<slot name="button" {show} {toggle} />
-	<div class="toolbar-panel">
-		<div class="toolbar-content" class:row class:column>
+<div class="toolbar" class:top class:bottom class:left class:right class:show on:click={onClick} bind:this={toolbar}>
+	<slot>
+		<slot name="button" {show} {toggle} />
+		<div class="toolbar-panel">
 			<slot name="panel" {toggle} />
 		</div>
-	</div>
+	</slot>
 </div>
 
 <style lang="less">
@@ -39,21 +46,53 @@
 		opacity: 0.3;
 		&:hover,
 		&:active,
+		&:focus-within,
 		&.show {
 			opacity: 1;
 		}
 
+		.toolbar-panel {
+			position: absolute;
+			overflow: hidden;
+			overflow-y: scroll;
+			border: 1px solid black;
+			border-radius: 4px;
+			background-color: white;
+			box-shadow: 0 4px 4px 0 fade(black, 10%);
+			opacity: 0;
+			max-height: 0;
+			pointer-events: none;
+			transition: all 0.2s ease-in-out;
+		}
+
+		&.show .toolbar-panel {
+			opacity: 1;
+			max-height: 80vh;
+			pointer-events: auto;
+		}
+
 		&.top {
 			top: 0px;
+			.toolbar-panel {
+				top: 100%;
+				margin-top: 8px;
+			}
 		}
 		&.left {
 			left: 0px;
 		}
 		&.right {
 			right: 0px;
+			.toolbar-panel {
+				right: -8px;
+			}
 		}
 		&.bottom {
 			bottom: 0px;
+			.toolbar-panel {
+				bottom: 100%;
+				margin-bottom: 12px;
+			}
 		}
 
 		:global(a) {
@@ -63,49 +102,6 @@
 		:global(button) {
 			cursor: pointer;
 			user-select: none;
-		}
-
-		.toolbar-panel {
-			position: absolute;
-			max-height: 0;
-			overflow: hidden;
-			overflow-y: scroll;
-		}
-
-		&.right {
-			.toolbar-panel {
-				right: 0;
-			}
-		}
-		&.top {
-			.toolbar-panel {
-				top: 100%;
-				margin-top: 12px;
-			}
-		}
-
-		&.show .toolbar-panel {
-			max-height: 80vh;
-			box-shadow: 0 4px 4px 0 fade(black, 10%);
-		}
-
-		.toolbar-content {
-			padding: 16px;
-			border: 1px solid black;
-			background-color: fade(white, 80%);
-			border-radius: 4px;
-
-			display: flex;
-			justify-content: center;
-			align-items: stretch;
-			gap: 24px;
-
-			&.row {
-				flex-direction: row;
-			}
-			&.column {
-				flex-direction: column;
-			}
 		}
 	}
 </style>
