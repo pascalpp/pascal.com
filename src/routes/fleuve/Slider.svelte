d4cccfad7f087eb99a7b575c9aae42f0c07f8d4d
Pascal <pascal@pascal.com>
2023-09-19 10:58:23 -0400

    improved slider

--- src/routes/fleuve/Slider.svelte
+++ src/routes/fleuve/Slider.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Column from '$lib/components/Column.svelte';
-
 	export let id: string;
 	export let value: number;
 	export let min: number = 0;
@@ -9,21 +7,36 @@
 	export let label: string;
 	export let title: string | undefined = undefined;
 
+	let input: HTMLInputElement;
+
 	$: labelValue = value.toFixed(step.toString().split('.')[1]?.length ?? 0);
 </script>
 
-<Column center gap="4px" {title}>
+<fieldet class="slider" {title} on:click={() => input.focus()}>
 	<label for={id}>{label} {labelValue}</label>
-	<input {id} type="range" {min} {max} {step} bind:value />
-</Column>
+	<input {id} type="range" {min} {max} {step} bind:value bind:this={input} />
+</fieldet>
 
 <style lang="less">
-	&:active,
-	&:focus {
-		outline: none;
-		&::-webkit-slider-thumb {
-			outline: 2px solid black;
-			border-radius: 50%;
+	.slider {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 4px;
+
+		input {
+			&::-webkit-slider-thumb {
+				outline: 1px solid black;
+				border-radius: 50%;
+			}
+
+			&:active,
+			&:focus {
+				outline: none;
+				&::-webkit-slider-thumb {
+					outline: 2px solid black;
+				}
+			}
 		}
 	}
 </style>
