2db393a4d7d601107ecc6d14d20d88e2337d7bdd
Pascal <pascal@pascal.com>
2023-09-20 18:32:34 -0400

    add analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,8 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
+
 export const prerender = true;
 export const trailingSlash = 'always';
 
