9f3304d2e59670bb9d748b8ac0d28cd8e5f92a65
Pascal <pascal@pascal.com>
2023-09-22 13:21:11 -0400

    first pass at writing tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,52 @@
+import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
+
+// set custom viewport for all desktop browsers.
+// mobile devices will use their own default viewport.
+const desktopOptions = {
+	viewport: { width: 1024, height: 720 },
+};
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	fullyParallel: true,
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+				...desktopOptions,
+			},
+		},
+
+		{
+			name: 'firefox',
+			use: {
+				...devices['Desktop Firefox'],
+				...desktopOptions,
+			},
+		},
+
+		{
+			name: 'safari',
+			use: {
+				...devices['Desktop Safari'],
+				...desktopOptions,
+			},
+		},
+
+		{
+			name: 'mobile-safari',
+			use: {
+				...devices['iPhone 12'],
+			},
+		},
+	],
 };
 
 export default config;
