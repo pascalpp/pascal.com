3d417486222d0588d90cffdb050705f2961eeefb
Pascal <pascal@pascal.com>
2023-09-23 17:23:39 -0400

    test cleanup

--- tests/fleuve/settings.test.ts
+++ tests/fleuve/settings.test.ts
@@ -19,15 +19,16 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Settings', async ({ page }) => {
-	await expect(page.getByLabel('Settings button')).toBeAttached();
-	await expect(page.getByLabel('Settings button')).toBeVisible();
-	await expect(page.getByLabel('Settings menu')).toBeAttached();
-	await expect(page.getByLabel('Settings menu')).not.toBeVisible();
+	const settingsButton = page.getByLabel('Settings button');
+	const settingsMenu = page.getByLabel('Settings menu');
+	await expect(settingsButton).toBeAttached();
+	await expect(settingsButton).toBeVisible();
+	await expect(settingsMenu).toBeAttached();
+	await expect(settingsMenu).not.toBeVisible();
 
-	await page.getByLabel('Settings button').click();
-	await expect(page.getByLabel('Settings menu')).toBeVisible();
-
-	await expect(page.getByLabel('Settings menu')).toHaveScreenshot('settings-panel.png', { scale: 'device' });
+	await settingsButton.click();
+	await expect(settingsMenu).toBeVisible();
+	await expect(settingsMenu).toHaveScreenshot('settings-panel.png', { scale: 'device' });
 
 	await page.getByLabel('Flow alignment: center').click();
 	await expect(page).toHaveScreenshot('flow-alignment-center.png', { scale: 'device' });
@@ -35,6 +36,6 @@ test('Settings', async ({ page }) => {
 	await page.getByLabel('Flow alignment: top').click();
 	await expect(page).toHaveScreenshot('flow-alignment-top.png', { scale: 'device' });
 
-	await page.getByLabel('Settings button').click();
-	await expect(page.getByLabel('Settings menu')).not.toBeVisible();
+	await settingsButton.click();
+	await expect(settingsMenu).not.toBeVisible();
 });
--- tests/fleuve/tutorial.test.ts
+++ tests/fleuve/tutorial.test.ts
@@ -19,137 +19,165 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Tutorial flow', async ({ page }) => {
-	await expect(page.getByTestId('Fleuve')).toHaveScreenshot('fleuve.png', { scale: 'device' });
+	const fleuveCard = page.getByTestId('Fleuve');
+	expect(fleuveCard).toBeFocused();
+	await expect(fleuveCard).toHaveScreenshot('fleuve.png', { scale: 'device' });
 
-	// focus the first card
-	await page.getByTestId('Fleuve').press('ArrowRight');
-	expect(page.getByTestId('Fleuve')).not.toBeFocused();
-	expect(page.getByTestId('This is a card')).toBeFocused();
-	expect(page.getByTestId('This is a card')).not.toHaveClass(/active/);
+	// focus the first child card
+	const firstCard = page.getByTestId('This is a card');
+	await page.keyboard.press('ArrowRight');
+	expect(fleuveCard).not.toBeFocused();
+	expect(firstCard).toBeFocused();
+	expect(firstCard).not.toHaveClass(/active/);
 
 	// activate the first card
-	await page.getByTestId('This is a card').press('ArrowRight');
-	expect(page.getByTestId('This is a card')).toHaveClass(/active/);
-	expect(page.getByTestId('Fleuve')).toHaveClass(/active/);
-	await expect(page.getByTestId('This is a card')).toHaveScreenshot('card-1.png', { scale: 'device' });
+	await page.keyboard.press('ArrowRight');
+	expect(firstCard).toHaveClass(/active/);
+	expect(fleuveCard).toHaveClass(/active/);
+	await expect(firstCard).toHaveScreenshot('card-1.png', { scale: 'device' });
 
 	// activate the second card
-	await page.getByTestId('This is a card').press('ArrowDown');
-	await page.getByTestId('A sibling card').press('Enter');
-	expect(page.getByTestId('A sibling card')).toBeFocused();
-	expect(page.getByTestId('A sibling card')).toHaveClass(/active/);
-	expect(page.getByTestId('This is a card')).not.toHaveClass(/active/);
+	const siblingCard = page.getByTestId('A sibling card');
+	await page.keyboard.press('ArrowDown');
+	await page.keyboard.press('Enter');
+	expect(siblingCard).toBeFocused();
+	expect(siblingCard).toHaveClass(/active/);
+	expect(firstCard).not.toHaveClass(/active/);
 
 	// activate the editing card
-	await page.getByTestId('A sibling card').press('ArrowDown');
-	await page.getByTestId('Editing cards').press('Enter');
-	expect(page.getByTestId('Editing cards')).toBeFocused();
-	expect(page.getByTestId('Editing cards')).toHaveClass(/active/);
-	expect(page.getByTestId('A sibling card')).not.toHaveClass(/active/);
+	const editingCard = page.getByTestId('Editing cards');
+	await page.keyboard.press('ArrowDown');
+	await page.keyboard.press('Enter');
+	expect(editingCard).toBeFocused();
+	expect(editingCard).toHaveClass(/active/);
+	expect(siblingCard).not.toHaveClass(/active/);
 
 	// edit the title
-	await page.getByTestId('Editing cards').press('Tab');
-	await expect(page.getByTestId('Editing cards').getByTestId('Title').getByRole('textbox')).toBeFocused();
-	await page.getByTestId('Editing cards').getByTestId('Title').getByRole('textbox').pressSequentially('A new title');
-	await page.getByTestId('Editing cards').getByTestId('Title').getByRole('textbox').press('Enter');
-	await expect(page.getByTestId('A new title')).toBeFocused(); // await for test id to change
+	const titleEditor = editingCard.getByTestId('Title').getByRole('textbox');
+	await page.keyboard.press('Tab');
+	await expect(titleEditor).toBeFocused();
+	await titleEditor.pressSequentially('A new title');
+	await titleEditor.press('Enter');
+	const updatedEditngCard = page.getByTestId('A new title');
+	await expect(updatedEditngCard).toBeFocused(); // await for test id to change
 
 	// edit the description
-	// await page.keyboard.press('Tab');
-	// await page.keyboard.press('Tab');
-	// await page.keyboard.press('Enter');
 	await page.keyboard.press('d');
-	await expect(page.getByTestId('A new title').getByTestId('Description').getByRole('textbox')).toBeFocused();
-	await page.getByTestId('A new title').getByTestId('Description').getByRole('textbox').press('Escape');
-	await expect(page.getByTestId('A new title').getByTestId('Description').getByRole('textbox')).not.toBeAttached();
-	await expect(page.getByTestId('A new title')).toBeFocused();
+	const descriptionEditor = updatedEditngCard.getByTestId('Description').getByRole('textbox');
+	await expect(descriptionEditor).toBeFocused();
+	await page.keyboard.press('Escape');
+	await expect(descriptionEditor).not.toBeAttached();
+	await expect(updatedEditngCard).toBeFocused();
 
 	// drill down the levels
-	await page.getByTestId('A new title').press('ArrowDown');
-	await expect(page.getByTestId('A new title')).not.toBeFocused();
-	await expect(page.getByTestId('Infinite levels')).toBeFocused();
+	const level1 = page.getByTestId('Infinite levels');
+	const level2 = page.getByTestId('Next level');
+	const level3 = page.getByTestId('Another level');
+	const level4 = page.getByTestId('Deeper still');
+
+	await page.keyboard.press('ArrowDown');
+	await expect(updatedEditngCard).not.toBeFocused();
+	await expect(level1).toBeFocused();
 	await page.keyboard.press('ArrowRight');
 	await page.keyboard.press('ArrowRight');
 	await page.keyboard.press('ArrowRight');
 	await page.keyboard.press('ArrowRight');
-	await expect(page.getByTestId('Deeper still')).toBeFocused();
-	await expect(page.getByTestId('Deeper still')).toHaveClass(/active/);
-	await expect(page.getByTestId('Another level')).toHaveClass(/active/);
-	await expect(page.getByTestId('Next level')).toHaveClass(/active/);
-	await expect(page.getByTestId('Infinite levels')).toHaveClass(/active/);
+	await expect(level1).toHaveClass(/active/);
+	await expect(level2).toHaveClass(/active/);
+	await expect(level3).toHaveClass(/active/);
+	await expect(level4).toHaveClass(/active/);
+	await expect(level4).toBeFocused();
 	await page.keyboard.press('ArrowLeft');
 	await page.keyboard.press('ArrowLeft');
 	await page.keyboard.press('ArrowLeft');
-	await expect(page.getByTestId('Infinite levels')).toBeFocused();
+	await expect(level1).toBeFocused();
 
 	// delete a card
-	await page.getByTestId('Infinite levels').press('ArrowDown');
-	await page.getByTestId('Deleting cards').press('Enter');
-	await expect(page.getByTestId('Deleting cards')).toBeFocused();
-	await expect(page.getByTestId('Deleting cards')).toHaveClass(/active/);
-	await expect(page.getByTestId('I’ll be deleted too!')).toBeAttached();
+	const deleteCard = page.getByTestId('Deleting cards');
+	const deleteChildCard = page.getByTestId('I’ll be deleted too!');
+	const reorderCard = page.getByTestId('Reorder cards');
+
+	await page.keyboard.press('ArrowDown');
+	await page.keyboard.press('Enter');
+	await expect(deleteCard).toBeFocused();
+	await expect(deleteCard).toHaveClass(/active/);
+	await expect(deleteChildCard).toBeAttached();
 	//register dialog handler before invoking the dialog
 	page.once('dialog', (dialog) => dialog.accept());
-	await page.getByTestId('Deleting cards').press('Backspace');
-	await expect(page.getByTestId('Deleting cards')).not.toBeAttached();
-	await expect(page.getByTestId('I’ll be deleted too!')).not.toBeAttached();
-	await expect(page.getByTestId('Reorder cards')).toBeFocused();
+	await page.keyboard.press('Backspace');
+	await expect(deleteCard).not.toBeAttached();
+	await expect(deleteChildCard).not.toBeAttached();
+	await expect(reorderCard).toBeFocused();
 
 	// reorder cards
-	await page.getByTestId('Reorder cards').press('Enter');
+	const rearrangeCard = page.getByTestId('Rearrange me');
+	const insertCard = page.getByTestId('Insert cards');
+	await page.keyboard.press('Enter');
 	await page.keyboard.press('ArrowRight');
 	await page.keyboard.press('Enter');
+	await expect(rearrangeCard).toBeFocused();
+	await expect(rearrangeCard).toHaveClass(/active/);
+
 	await page.keyboard.press('Shift+ArrowDown');
 	await expect(page).toHaveScreenshot('reorder-1.png', { scale: 'device' });
 	await page.keyboard.press('Shift+ArrowRight');
-	await expect(page.getByTestId('Insert cards')).toHaveClass(/active/);
+	await expect(insertCard).toHaveClass(/active/);
 	await expect(page).toHaveScreenshot('reorder-2.png', { scale: 'device' });
 	await page.keyboard.press('Shift+ArrowLeft');
-	await expect(page.getByTestId('Insert cards')).not.toHaveClass(/active/);
+	await expect(insertCard).not.toHaveClass(/active/);
 	await expect(page).toHaveScreenshot('reorder-3.png', { scale: 'device' });
 	await page.keyboard.press('Shift+ArrowUp');
 	await expect(page).toHaveScreenshot('reorder-4.png', { scale: 'device' });
 
 	// insert cards
 	await page.keyboard.press('ArrowDown');
-	await page.getByTestId('Insert cards').press('Enter');
-	await expect(page.getByTestId('Insert cards')).toHaveClass(/active/);
+	await page.keyboard.press('Enter');
+	await expect(insertCard).toBeFocused();
+	await expect(insertCard).toHaveClass(/active/);
 	await page.keyboard.press('Alt+ArrowRight');
-	await expect(page.getByTestId('Untitled card')).toBeAttached();
-	await expect(page.getByTestId('Untitled card')).toHaveClass(/active/);
-	await expect(page.getByTestId('Insert cards')).toBeFocused();
+	const untitledCard = page.getByTestId('Untitled card');
+	await expect(untitledCard).toBeAttached();
+	await expect(untitledCard).toHaveClass(/active/);
+	await expect(insertCard).toBeFocused();
 	await page.keyboard.press('Alt+ArrowLeft');
-	await expect(page.getByTestId('Untitled card')).not.toBeAttached();
-	await expect(page.getByTestId('Insert cards')).toBeFocused();
+	await expect(untitledCard).not.toBeAttached();
+	await expect(insertCard).toBeFocused();
+	await page.keyboard.press('ArrowLeft');
+	await expect(reorderCard).toBeFocused();
 
 	// add a child card
-	await page.keyboard.press('ArrowLeft');
+	const addMoreCard = page.getByTestId('Add more cards');
 	await page.keyboard.press('ArrowDown');
 	await page.keyboard.press('Enter');
-	await expect(page.getByTestId('Add more cards')).toBeFocused();
-	await expect(page.getByTestId('Add more cards')).toHaveClass(/active/);
+	await expect(addMoreCard).toBeFocused();
+	await expect(addMoreCard).toHaveClass(/active/);
 	await page.keyboard.press('ArrowRight');
 	await page.keyboard.type('Another child card');
 	await page.keyboard.press('Enter');
-	await expect(page.getByTestId('Another child card')).toBeFocused();
-	await expect(page.getByTestId('Another child card')).toHaveClass(/active/);
+	const addedChildCard = page.getByTestId('Another child card');
+	await expect(addedChildCard).toBeFocused();
+	await expect(addedChildCard).toHaveClass(/active/);
+	await page.keyboard.press('ArrowLeft');
+	await expect(addMoreCard).toBeFocused();
 
 	// add a sibling card
-	await page.keyboard.press('ArrowLeft');
 	await page.keyboard.press('ArrowDown');
 	await page.keyboard.type('Another sibling card');
 	await page.keyboard.press('Enter');
-	await expect(page.getByTestId('Another sibling card')).toBeFocused();
-	await expect(page.getByTestId('Another sibling card')).toHaveClass(/active/);
+	const addedSiblingCard = page.getByTestId('Another sibling card');
+	await expect(addedSiblingCard).toBeFocused();
+	await expect(addedSiblingCard).toHaveClass(/active/);
 
 	// delete tutorial flow
 	await page.keyboard.press('ArrowLeft');
-	await expect(page.getByTestId('Fleuve')).toBeFocused();
+	await expect(fleuveCard).toBeFocused();
+	// test that canceling the dialog does not delete the card
 	page.once('dialog', (dialog) => dialog.dismiss());
 	await page.keyboard.press('Backspace');
-	await expect(page.getByTestId('Fleuve')).toBeAttached();
+	await expect(fleuveCard).toBeAttached();
+	// test that accepting the dialog deletes the card
 	page.once('dialog', (dialog) => dialog.accept());
 	await page.keyboard.press('Backspace');
-	await expect(page.getByTestId('Fleuve')).not.toBeAttached();
+	await expect(fleuveCard).not.toBeAttached();
 	await expect(page).toHaveScreenshot('deleted-tutorial.png', { scale: 'device' });
 });
