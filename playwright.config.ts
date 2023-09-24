7007557c1b3cc8c68a63b1a3d03327857999f73e
Pascal <pascal@pascal.com>
2023-09-24 00:30:04 -0400

    update playwright config to support slowmo

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,108 +6,108 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config();
 const desktopOptions = {
-	viewport: { width: 1024, height: 800 },
+  viewport: { width: 1024, height: 800 },
 };
+
+const slowMo = Number(process.env.SLOW) || 0;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	webServer: process.env.CI
-		? {
-				command: 'npm run build && npm run preview',
-				port: 4173,
-		  }
-		: undefined,
-	testDir: './tests',
-	/* Run tests in files in parallel */
-	fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'html' : 'list',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: process.env.CI
-			? undefined
-			: process.env.PRODUCTION
-			? 'https://www.pascal.com'
-			: 'https://pascal.localhost',
+  webServer: process.env.CI
+    ? {
+        command: 'npm run build && npm run preview',
+        port: 4173,
+      }
+    : undefined,
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI ? 'html' : 'list',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.CI
+      ? undefined
+      : process.env.PRODUCTION
+      ? 'https://www.pascal.com'
+      : 'https://pascal.localhost',
 
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
 
-		launchOptions: process.env.CI
-			? undefined
-			: {
-					slowMo: 250,
-					timeout: 300000,
-			  },
-	},
+    launchOptions: {
+      slowMo,
+      timeout: slowMo * 30 + 30000,
+    },
+  },
 
-	/* Configure projects for major browsers */
-	projects: [
-		{
-			name: 'chromium',
-			use: {
-				...devices['Desktop Chrome'],
-				...desktopOptions,
-			},
-		},
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        ...desktopOptions,
+      },
+    },
 
-		// {
-		// 	name: 'firefox',
-		// 	use: {
-		// 		...devices['Desktop Firefox'],
-		// 		...desktopOptions,
-		// 	},
-		// },
+    // {
+    // 	name: 'firefox',
+    // 	use: {
+    // 		...devices['Desktop Firefox'],
+    // 		...desktopOptions,
+    // 	},
+    // },
 
-		{
-			name: 'safari',
-			use: {
-				...devices['Desktop Safari'],
-				...desktopOptions,
-			},
-		},
+    {
+      name: 'safari',
+      use: {
+        ...devices['Desktop Safari'],
+        ...desktopOptions,
+      },
+    },
 
-		// {
-		// 	name: 'iphone',
-		// 	use: {
-		// 		...devices['iPhone 12'],
-		// 	},
-		// },
+    // {
+    // 	name: 'iphone',
+    // 	use: {
+    // 		...devices['iPhone 12'],
+    // 	},
+    // },
 
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
-	],
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
 
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
