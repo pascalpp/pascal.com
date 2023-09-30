0d1ce92d771e5c5fc471bd79729f1c9baf7cd740
Pascal <pascal@pascal.com>
2023-09-30 11:51:38 -0400

    Update util.ts

--- src/routes/api/posts/util.ts
+++ src/routes/api/posts/util.ts
@@ -1,51 +1,58 @@
 import { dev } from '$app/environment';
 import type { SvelteComponent } from 'svelte';
 
-type PostResolver = () => Promise<{ metadata: Record<string, string> }>;
+export type PostMetadata = {
+  title: string;
+  date: string;
+  status: string;
+  tags?: string[];
+};
 
 export type PostSummary = {
-	slug: string;
-	path: string;
-	metadata: Record<string, string>;
+  slug: string;
+  path: string;
+  metadata: PostMetadata;
 };
 
 export type Post = {
-	metadata: Record<string, string>;
-	default: SvelteComponent;
+  metadata: PostMetadata;
+  default: SvelteComponent;
 };
 
+type PostResolver = () => Promise<{ metadata: PostMetadata }>;
+
 export async function fetchAllPosts(): Promise<PostSummary[]> {
-	const files = import.meta.glob('/src/routes/diary/**/page.md');
-	const iterableFiles = Object.entries(files) as [string, PostResolver][];
+  const files = import.meta.glob('/src/routes/diary/**/page.md');
+  const iterableFiles = Object.entries(files) as [string, PostResolver][];
 
-	const posts = await Promise.all(
-		iterableFiles.map(async ([path, post]) => {
-			const { metadata } = await post();
-			const slug = path.split('/').slice(-2)[0];
-			return {
-				slug,
-				path,
-				metadata,
-			};
-		})
-	);
+  const posts = await Promise.all(
+    iterableFiles.map(async ([path, post]) => {
+      const { metadata } = await post();
+      const slug = path.split('/').slice(-2)[0];
+      return {
+        slug,
+        path,
+        metadata: metadata as PostMetadata,
+      };
+    })
+  );
 
-	if (dev) {
-		return posts;
-	} else {
-		return posts.filter((post) => post.metadata.status !== 'draft');
-	}
+  if (dev) {
+    return posts;
+  } else {
+    return posts.filter((post) => post.metadata.status !== 'draft');
+  }
 }
 
 export async function fetchPost(slug: string): Promise<Post> {
-	const post = (await import(`../../diary/${slug}/page.md`)) as Post;
-	return post;
+  const post = (await import(`../../diary/${slug}/page.md`)) as Post;
+  return post;
 }
 
 export async function fetchAllPostsByTag(tag: string): Promise<PostSummary[]> {
-	const posts = await fetchAllPosts();
-	const filteredPosts = posts.filter((post) => {
-		return post.metadata.tags?.includes(tag);
-	});
-	return filteredPosts;
+  const posts = await fetchAllPosts();
+  const filteredPosts = posts.filter((post) => {
+    return post.metadata.tags?.includes(tag);
+  });
+  return filteredPosts;
 }
