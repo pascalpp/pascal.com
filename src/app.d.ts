8ad8a4800ced70ac102b21b57675a1b91637cc43
Pascal <pascal@pascal.com>
2023-09-30 11:50:13 -0400

    prettier

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,23 +1,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
 }
 
 declare module '*.md' {
-	import type { SvelteComponent } from 'svelte';
+  import type { SvelteComponent } from 'svelte';
 
-	export default class Comp extends SvelteComponent {}
-	export const metadata: Record<string, string>;
+  export default class Comp extends SvelteComponent {}
+  export const metadata: Record<string, string>;
 }
 
 declare module '*.svg?component' {
-	import type { SvelteComponent } from 'svelte';
+  import type { SvelteComponent } from 'svelte';
 
-	export default class Comp extends SvelteComponent {}
+  export default class Comp extends SvelteComponent {}
 }
