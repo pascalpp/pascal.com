1db2208fab5a68067f43a52e7b1ca1d86d421159
Pascal <pascal@pascal.com>
2023-10-07 18:34:50 -0400

    add diary error page

---
+++ src/routes/diary/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import PostList from './PostList.svelte';
+</script>
+
+<article>
+  <h2>Post not found.</h2>
+</article>
+
+<style lang="less">
+  article {
+    font-size: 20px;
+    font-weight: 200;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
