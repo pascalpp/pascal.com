bed7b2caf11b2391e74417ae53b08f036dab6a5b
Pascal <pascal@pascal.com>
2023-10-07 19:00:30 -0400

    remove dumb diary intro graf

--- src/routes/diary/+page.svelte
+++ src/routes/diary/+page.svelte
@@ -5,25 +5,11 @@
   $: ({ posts } = data);
 </script>
 
-<header>
-  <p>
-    Most of the posts below have been resurrected from an old Movable Type blog I wrote in the early 2000s. I'm also
-    gradually re-adding the posts from my more recent Wordpress blog, and maybe I’ll write some new posts from time to
-    time.
-  </p>
-</header>
-
 <article>
   <PostList {posts} />
 </article>
 
 <style lang="less">
-  header {
-    margin-bottom: 1em;
-    font-size: 20px;
-    font-weight: 200;
-  }
-
   article {
     font-size: 20px;
     font-weight: 200;
