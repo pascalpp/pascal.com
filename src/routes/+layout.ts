5a2817e93f242dd55d6ffcbe4544f84eb08e4d52
Pascal <pascal@pascal.com>
2023-10-29 00:20:02 -0400

    turn off trailing slash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,7 @@ import { inject } from '@vercel/analytics';
 inject({ mode: dev ? 'development' : 'production' });
 
 export const prerender = true;
-export const trailingSlash = 'always';
+// export const trailingSlash = 'always';
 
 export const load = ({ url }) => {
   const { pathname } = url;
