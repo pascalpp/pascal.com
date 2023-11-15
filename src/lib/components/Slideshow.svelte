e57d79d5acc99e8b564f0d79d372a5b721ea7312
Pascal <pascal@pascal.com>
2023-11-15 10:40:37 -0500

    add zoom click handler

--- src/lib/components/Slideshow.svelte
+++ src/lib/components/Slideshow.svelte
@@ -7,19 +7,45 @@
 </script>
 
 <script lang="ts">
+  import Dialog from './Dialog.svelte';
+
   export let slides: Slide[] = [];
 
-  function scrollIntoView(node: HTMLElement) {
-    console.log('scrollIntoView');
-    node.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+  let zoomSrc: string | undefined = undefined;
+  let zoomAlt: string | undefined = undefined;
+
+  let openZoom = () => {};
+
+  function scrollIntoView(element: HTMLElement) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
   }
 
-  function useScrollIntoView(node: HTMLElement) {
-    const onClick = () => scrollIntoView(node);
-    node.addEventListener('click', onClick);
+  function elementIsInView(element: HTMLElement) {
+    const rect = element.getBoundingClientRect();
+    const values = [rect.top < 0, rect.bottom > window.innerHeight, rect.left < 0, rect.right > window.innerWidth];
+    return !values.some((value) => value);
+  }
+
+  function zoomImage(element: HTMLElement) {
+    const img = element.querySelector('img');
+    zoomSrc = img?.src;
+    zoomAlt = img?.alt;
+    openZoom();
+  }
+
+  function useScrollIntoView(element: HTMLElement) {
+    const onClick = () => {
+      if (elementIsInView(element)) {
+        zoomImage(element);
+      } else {
+        scrollIntoView(element);
+      }
+    };
+
+    element.addEventListener('click', onClick);
     return {
       destroy() {
-        node.removeEventListener('click', onClick);
+        element.removeEventListener('click', onClick);
       },
     };
   }
@@ -36,27 +62,36 @@
   </ul>
 </div>
 
+<Dialog bind:open={openZoom}>
+  <img src={zoomSrc} alt={zoomAlt} />
+</Dialog>
+
 <style lang="less">
+  .slideshow {
+    width: 100%;
+    height: 100%;
+  }
   .slides {
     overflow: hidden;
     overflow-x: auto;
     list-style: none;
     display: flex;
     flex-direction: row;
-    gap: 1rem;
-    padding: 0 1rem;
+    gap: 2rem;
+    padding: 2rem;
+    padding-top: 0;
     scroll-snap-type: x mandatory;
   }
   .slide {
     flex-shrink: 0;
-    width: 600px;
+    width: 60%;
     scroll-snap-align: center;
     user-select: none;
 
     img {
       width: 100%;
       height: auto;
-      border: 1px solid rgba(0 0 0 / 0.1);
+      border: 1px solid rgba(0 0 0 / 0.3);
       box-shadow: 0 2px 4px rgba(0 0 0 / 0.1);
       border-radius: 3px;
     }
