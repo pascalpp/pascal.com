80d6e214c9ec50079a56c27259a769e8d94a7f0a
Pascal <pascal@pascal.com>
2023-11-19 15:19:36 -0500

    add slides tests and some working shots

--- tests/dev/dev.test.ts
+++ tests/dev/dev.test.ts
@@ -15,6 +15,60 @@ test('Resume', async ({ page }) => {
     scale: 'device',
     fullPage: true,
     maxDiffPixelRatio: 0,
-    threshold: 0,
+    threshold: 0.1,
   });
+});
+
+test('Resume - slides', async ({ page }) => {
+  // broadly
+  const recentSection = page.getByLabel('Recent experience');
+  expect(recentSection).toBeVisible();
+  await recentSection.hover();
+  await recentSection.focus();
+  await recentSection.click();
+
+  const broadlySlidesLink = recentSection.getByLabel('View Broadly slides');
+  expect(broadlySlidesLink).toBeVisible();
+  await broadlySlidesLink.hover();
+  await broadlySlidesLink.focus();
+  await broadlySlidesLink.click();
+
+  expect(page.locator('dialog[open]')).toBeVisible();
+
+  await expect(page).toHaveScreenshot('broadly-slides.png', {
+    scale: 'device',
+    fullPage: false,
+    maxDiffPixelRatio: 0,
+    threshold: 0.1,
+  });
+
+  await page.keyboard.press('Escape');
+  await page.waitForTimeout(1000);
+
+  // about.me
+  const pastSection = page.getByLabel('Past experience');
+  expect(pastSection).toBeVisible();
+  await pastSection.hover();
+  await pastSection.focus();
+  await pastSection.click();
+
+  const aboutmeSlidesLink = pastSection.getByLabel('View about.me slides');
+  expect(aboutmeSlidesLink).toBeVisible();
+  await aboutmeSlidesLink.hover();
+  await aboutmeSlidesLink.focus();
+  await aboutmeSlidesLink.click();
+
+  await page.waitForTimeout(250);
+  await page.waitForLoadState('networkidle');
+  expect(page.locator('dialog[open]')).toBeVisible();
+
+  await expect(page).toHaveScreenshot('aboutme-slides.png', {
+    scale: 'device',
+    fullPage: false,
+    maxDiffPixelRatio: 0,
+    threshold: 0.1,
+  });
+
+  await page.keyboard.press('Escape');
+  await page.waitForTimeout(1000);
 });
