966a0c241a082a30aa2efd9adad0cdf7edc594ef
Pascal <pascal@pascal.com>
2023-11-20 13:58:07 -0500

    add container queries post

---
+++ src/routes/diary/2023-11-20-container-queries/Box.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  export let title = 'Container queries';
+</script>
+
+<div class="box">
+  <h1>{title}</h1>
+  <p class="content40">0ch ↔ 40ch</p>
+  <p class="content40-80">40ch ↔ 80ch</p>
+  <p class="content80-100">80ch ↔ 100ch</p>
+  <p class="content100-120">100ch ↔ 120ch</p>
+  <p class="content120">&gt; 120ch</p>
+</div>
+
+<style lang="less">
+  .box {
+    color: white;
+    padding: 1rem;
+    min-height: 100px;
+
+    h1 {
+      line-height: 1;
+      margin-bottom: 0.1em;
+    }
+
+    p {
+      display: none;
+    }
+  }
+
+  @container (inline-size <= 40ch) {
+    .box {
+      background-color: teal;
+      .content40 {
+        display: block;
+      }
+    }
+  }
+  @container (inline-size > 40ch) and (inline-size <= 80ch) {
+    .box {
+      background-color: rebeccapurple;
+      .content40-80 {
+        display: block;
+      }
+    }
+  }
+  @container (inline-size > 80ch) and (inline-size <= 100ch) {
+    .box {
+      background-color: salmon;
+      .content80-100 {
+        display: block;
+      }
+    }
+  }
+  @container (inline-size > 100ch) and (inline-size <= 120ch) {
+    .box {
+      background-color: darkgoldenrod;
+      .content100-120 {
+        display: block;
+      }
+    }
+  }
+  @container (inline-size > 120ch) {
+    .box {
+      background-color: maroon;
+      .content120 {
+        display: block;
+      }
+    }
+  }
+</style>
---
+++ src/routes/diary/2023-11-20-container-queries/page.md
@@ -0,0 +1,51 @@
+---
+title: Container queries
+date: 2023-11-20T17:53:31.546Z
+status: published
+tags: [css]
+---
+
+<script lang="ts">
+  import Box from './Box.svelte';
+</script>
+
+<style>
+  .container {
+    container-name: container;
+    container-type: inline-size;
+  }
+
+  .box {
+    background-color: purple;
+    color: white;
+    padding: 1rem;
+    min-height: 100px;
+  }
+
+  .inset {
+    max-width: 40ch;
+    margin-left: 4rem;
+  }
+</style>
+
+A little playground to experiment with container queries. Try resizing the window or switching to responsive mode in your browser.
+
+<div class="container">
+  <Box title="Content"/>
+</div>
+
+<div class="container inset">
+  <Box title="Inset"/>
+</div>
+
+<div class="container bleed-left">
+  <Box title="Bleed left"/>
+</div>
+
+<div class="container bleed-right">
+  <Box title="Bleed right"/>
+</div>
+
+<div class="container bleed">
+  <Box title="Bleed"/>
+</div>
