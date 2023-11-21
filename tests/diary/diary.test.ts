0462a652056aaf48817e34941a171193e3a3fd41
Pascal <pascal@pascal.com>
2023-11-21 10:58:52 -0500

    add container queries test

--- tests/diary/diary.test.ts
+++ tests/diary/diary.test.ts
@@ -18,3 +18,19 @@ test('CSS Grid Layout', async ({ page }) => {
     threshold: 0.1,
   });
 });
+
+test('Container queries', async ({ page }) => {
+  const viewport = page.viewportSize();
+  if (viewport && viewport.width > 1000) {
+    await page.setViewportSize({ width: 1400, height: viewport?.height });
+  }
+
+  await page.goto('/diary/2023-11-20-container-queries');
+
+  await expect(page).toHaveScreenshot('container-queries.png', {
+    scale: 'device',
+    fullPage: true,
+    maxDiffPixelRatio: 0.1,
+    threshold: 0.1,
+  });
+});
