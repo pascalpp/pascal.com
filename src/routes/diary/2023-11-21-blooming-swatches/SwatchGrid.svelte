f3ded1ff78ee5db47c5e94449afe1a71393a0c9f
Pascal <pascal@pascal.com>
2023-11-21 11:59:27 -0500

    let swatch generate its own colors

--- src/routes/diary/2023-11-21-blooming-swatches/SwatchGrid.svelte
+++ src/routes/diary/2023-11-21-blooming-swatches/SwatchGrid.svelte
@@ -1,28 +1,18 @@
-<script lang="ts" context="module">
-  type SwatchColors = {
-    color: string;
-    hovercolor: string;
-  };
-</script>
-
 <script lang="ts">
   import Swatch from './Swatch.svelte';
-  import randomHexColor from './randomColor';
 
-  export let swatches: SwatchColors[] = new Array(100).fill(null).map((_, i) => ({
-    color: randomHexColor(),
-    hovercolor: randomHexColor(),
-  }));
+  export let swatches = new Array(100).fill(null);
 </script>
 
 <div class="swatch-grid">
-  {#each swatches as { color, hovercolor }}
-    <Swatch {color} {hovercolor} />
+  {#each swatches as _}
+    <Swatch />
   {/each}
 </div>
 
 <style lang="less">
   .swatch-grid {
+    margin-block: 1rem;
     display: grid;
     gap: 4px;
     grid-template-columns: repeat(10, 1fr);
