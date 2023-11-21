64c48029a6ccbc1a292b3ea1f06209f4c1496541
Pascal <pascal@pascal.com>
2023-11-21 11:59:10 -0500

    rotate colors on intervals

--- src/routes/diary/2023-11-21-blooming-swatches/Swatch.svelte
+++ src/routes/diary/2023-11-21-blooming-swatches/Swatch.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
   import randomHexColor from './randomColor';
 
-  export let color = 'red';
-  export let hovercolor = 'blue';
+  let color = randomHexColor();
+  let hovercolor = randomHexColor();
+  let endcolor = randomHexColor();
   let animating = false;
 
   function startAnimating(event: MouseEvent | KeyboardEvent) {
@@ -13,15 +16,27 @@
   }
 
   function stopAnimating() {
-    color = randomHexColor();
+    color = endcolor;
     hovercolor = randomHexColor();
+    endcolor = randomHexColor();
     animating = false;
   }
+
+  function intervalColor() {
+    if (animating) return;
+    color = randomHexColor();
+  }
+
+  let intervalTime = Math.random() * 4000;
+  onMount(() => {
+    const interval = setInterval(intervalColor, intervalTime);
+    return () => clearInterval(interval);
+  });
 </script>
 
 <div
   class="swatch"
-  style="--color: {color}; --hovercolor: {hovercolor}"
+  style="--color: {color}; --hovercolor: {hovercolor}; --endcolor: {endcolor};"
   on:mouseenter={startAnimating}
   on:click={startAnimating}
   on:keypress={startAnimating}
@@ -33,27 +48,31 @@
 <style lang="less">
   .swatch {
     background-color: var(--color);
+    transition: background-color 2s ease-out;
     border: 1px solid rgba(0 0 0 / 0.2);
     border-radius: 4px;
     aspect-ratio: 1;
-    transition: background-color 4s ease-in-out;
     &.animating {
       pointer-events: none;
-      animation: bloom 1.5s ease-out forwards;
+      animation: bloom 2s ease-out forwards;
     }
   }
 
   @keyframes bloom {
-    0%,
-    100% {
+    0% {
       background-color: var(--color);
       scale: 1;
       opacity: 1;
     }
-    75% {
+    50% {
       background-color: var(--hovercolor);
       scale: 3;
-      opacity: 0;
+      opacity: 0.1;
+    }
+    100% {
+      background-color: var(--endcolor);
+      scale: 1;
+      opacity: 1;
     }
   }
 </style>
