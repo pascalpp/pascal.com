b47737cd238f54cb39da8bfe4be2601e75ed5ac6
Pascal <pascal@pascal.com>
2023-11-21 11:42:48 -0500

    update diary tests

--- tests/diary/diary.test.ts
+++ tests/diary/diary.test.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
   });
 });
 
-test('CSS Grid Layout', async ({ page }) => {
+test('2023-11-20-css-grid-layout', async ({ page }) => {
   await page.goto('/diary/2023-11-20-css-grid-layout');
 
   await expect(page).toHaveScreenshot('css-grid-layout.png', {
@@ -19,7 +19,7 @@ test('CSS Grid Layout', async ({ page }) => {
   });
 });
 
-test('Container queries', async ({ page }) => {
+test('2023-11-20-container-queries', async ({ page }) => {
   const viewport = page.viewportSize();
   if (viewport && viewport.width > 1000) {
     await page.setViewportSize({ width: 1400, height: viewport?.height });
@@ -28,6 +28,17 @@ test('Container queries', async ({ page }) => {
   await page.goto('/diary/2023-11-20-container-queries');
 
   await expect(page).toHaveScreenshot('container-queries.png', {
+    scale: 'device',
+    fullPage: true,
+    maxDiffPixelRatio: 0.1,
+    threshold: 0.1,
+  });
+});
+
+test('2010-01-28-iphone-multitasking', async ({ page }) => {
+  await page.goto('/diary/2010-01-28-iphone-multitasking');
+
+  await expect(page).toHaveScreenshot('iphone-multitasking.png', {
     scale: 'device',
     fullPage: true,
     maxDiffPixelRatio: 0.1,
