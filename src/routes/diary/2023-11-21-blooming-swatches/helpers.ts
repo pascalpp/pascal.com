861696a614634c8c9ea00348095d78b19e1e9c9f
Pascal <pascal@pascal.com>
2023-12-12 09:13:38 -0500

    Update helpers.ts

--- src/routes/diary/2023-11-21-blooming-swatches/helpers.ts
+++ src/routes/diary/2023-11-21-blooming-swatches/helpers.ts
@@ -1,5 +1,31 @@
 export function randomHexColor(): string {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  // const initial = Math.floor(Math.random() * 16777215).toString(16);
+  const randomR = randomHexPart();
+  const randomG = randomHexPart();
+  const randomB = randomHexPart();
+  const color = `#${randomR}${randomG}${randomB}`;
+  return color;
+}
+
+function randomHexPart() {
+  const hex = Math.floor(Math.random() * 255).toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
+}
+
+export function getYIQ(color: string) {
+  // remove hash from hexa string
+  const hexColor = color.substring(1);
+
+  const r = parseInt(hexColor.substring(0, 2), 16);
+  const g = parseInt(hexColor.substring(2, 4), 16);
+  const b = parseInt(hexColor.substring(4, 6), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq;
+}
+
+export function getContastingColor(color: string) {
+  const yiq = getYIQ(color);
+  return yiq >= 128 ? 'black' : 'white';
 }
 
 const letters = 'ABCDEFG';
