49a9fa2eb8d3ac35b0cf519237bf56ce9abb27c4
Pascal <pascal@pascal.com>
2024-03-31 00:25:35 -0400

    move PostListItem to separate component

--- src/routes/diary/PostList.svelte
+++ src/routes/diary/PostList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ButtonBar from '$lib/components/ButtonBar.svelte';
   import type { PostSummary } from '../api/posts/util';
+  import PostListItem from './PostListItem.svelte';
   import Tag from './Tag.svelte';
 
   export let posts: PostSummary[] = [];
@@ -58,19 +59,11 @@
   </div>
 </header>
 
-<ul>
+<ul class="post-list">
   {#each sorted as post}
-    {@const { metadata } = post}
-    {@const { title, summary, date } = metadata}
-    <li class="post">
-      <a href="/diary/{post.slug}">
-        <span class="date">{formatDate(date)}</span>
-        <span class="title">{title}</span>
-        {#if summary}
-          <span class="summary">{summary}</span>
-        {/if}
-      </a>
-    </li>
+    {#key post.slug}
+      <PostListItem {post} />
+    {/key}
   {/each}
 </ul>
 
@@ -82,6 +75,7 @@
     flex-direction: row;
     align-items: center;
     gap: 2em;
+    margin-inline: auto;
 
     @media @mobile {
       flex-direction: column;
@@ -104,82 +98,18 @@
     }
   }
 
-  ul,
-  li {
+  .post-list {
     list-style: none;
     margin: 0;
     padding: 0;
-  }
-
-  ul {
     margin-top: 2rem;
     display: flex;
     flex-direction: column;
     gap: 1.5em;
     padding-inline: 2rem;
-    max-width: 65ch;
     @media @mobile {
       flex-direction: column;
       grid-column: full-width;
-    }
-  }
-
-  .post {
-    width: max-content;
-    &:nth-child(even) {
-      margin: auto;
-    }
-    &:nth-child(3n + 1) {
-      --deg: -1.5deg;
-    }
-    &:nth-child(3n + 2) {
-      --deg: 0deg;
-    }
-    &:nth-child(3n + 3) {
-      --deg: 1.5deg;
-    }
-    &:nth-child(4n + 1) {
-      margin-left: -1em;
-    }
-    &:nth-child(4n + 4) {
-      margin-right: -1em;
-    }
-
-    --shadow-offset: 0.1em;
-    --shadow-size: -1px;
-    .rotated-shadow;
-
-    a {
-      max-width: min(50ch, 90vw);
-      padding: 0.75em 1em;
-      background-color: white;
-      border-radius: 2px;
-      text-decoration: none;
-      display: flex;
-      flex-direction: column;
-      gap: 0.25em;
-      line-height: 1.4;
-
-      .title {
-        text-wrap: balance;
-        &::after {
-          content: ' →';
-          font-family: var(--sans-font);
-        }
-      }
-
-      .summary {
-        font-size: 14px;
-        font-family: var(--sans-font);
-        color: rgba(0 0 0 / 0.75);
-        text-wrap: pretty;
-      }
-
-      .date {
-        color: rgba(0 0 0 / 0.75);
-        font-family: var(--sans-font);
-        font-size: 12px;
-      }
     }
   }
 </style>
---
+++ src/routes/diary/PostListItem.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import type { PostSummary } from '../api/posts/util';
+
+  export let post: PostSummary;
+
+  let { metadata } = post;
+  let { title, summary, date } = metadata;
+
+  const dateFormatter = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  function formatDate(timestamp: string) {
+    try {
+      const date = new Date(timestamp);
+      return dateFormatter.format(date);
+    } catch (e) {
+      console.log(timestamp);
+      return timestamp;
+    }
+  }
+</script>
+
+<li class="post">
+  <a href="/diary/{post.slug}">
+    <span class="date">{formatDate(date)}</span>
+    <span class="title">{title}</span>
+    {#if summary}
+      <span class="summary">{summary}</span>
+    {/if}
+  </a>
+</li>
+
+<style lang="less">
+  .post {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: max-content;
+    &:nth-child(even) {
+      margin: auto;
+    }
+    &:nth-child(3n + 1) {
+      --deg: -1.5deg;
+    }
+    &:nth-child(3n + 2) {
+      --deg: 0deg;
+    }
+    &:nth-child(3n + 3) {
+      --deg: 1.5deg;
+    }
+    &:nth-child(4n + 1) {
+      margin-left: -1em;
+    }
+    &:nth-child(4n + 4) {
+      margin-right: -1em;
+    }
+
+    --shadow-offset: 0.1em;
+    --shadow-size: -1px;
+    .rotated-shadow;
+
+    a {
+      max-width: min(50ch, 90vw);
+      padding: 0.75em 1em;
+      background-color: white;
+      border-radius: 2px;
+      text-decoration: none;
+      display: flex;
+      flex-direction: column;
+      gap: 0.25em;
+      line-height: 1.4;
+
+      .title {
+        text-wrap: balance;
+        &::after {
+          content: ' →';
+          font-family: var(--sans-font);
+        }
+      }
+
+      .summary {
+        font-size: 14px;
+        font-family: var(--sans-font);
+        color: rgba(0 0 0 / 0.75);
+        text-wrap: pretty;
+      }
+
+      .date {
+        color: rgba(0 0 0 / 0.75);
+        font-family: var(--sans-font);
+        font-size: 12px;
+      }
+    }
+  }
+</style>
