361f4cc307d0e9cedf69f250ba7362ec9cd3f46c
Pascal <pascal@pascal.com>
2024-05-03 23:20:23 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 declare module '*.md' {
   import type { SvelteComponent } from 'svelte';
-
-  export default class Comp extends SvelteComponent {}
+  export default class MarkdownComponent extends SvelteComponent {}
   export const metadata: Record<string, string>;
 }
 
