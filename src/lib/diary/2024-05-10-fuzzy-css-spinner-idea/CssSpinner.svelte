5762eec078981f804d5d2d903d4c8ac88a4f6ff1
Pascal <pascal@pascal.com>
2024-05-10 20:26:17 -0400

    first pass at fuzzy css spinner

---
+++ src/lib/diary/2024-05-10-fuzzy-css-spinner-idea/CssSpinner.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+</script>
+
+<div class="fuzzy-spinner">
+  <div class="spinner red" />
+  <div class="spinner blue" />
+  <div class="spinner green" />
+</div>
+
+<style lang="less">
+  .fuzzy-spinner {
+    width: 300px;
+    height: 300px;
+    position: relative;
+  }
+
+  .spinner {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    inset: 0;
+    animation: spin 9s linear infinite;
+    transition: border-width 0.5s;
+    --border1: 20px;
+    --border2: 35px;
+    --border3: 15px;
+    --border4: 5px;
+    --min-scale: 0.8;
+    --max-scale: 1;
+
+    &.red {
+      color: red;
+      --min-opacity: 0.8;
+      --max-opacity: 1;
+    }
+    &.blue {
+      color: blue;
+      --min-opacity: 0.3;
+      --max-opacity: 0.7;
+      &::before {
+        transform: rotate(120deg);
+        animation-delay: 3s;
+      }
+    }
+    &.green {
+      color: green;
+      --min-opacity: 0.1;
+      --max-opacity: 0.7;
+      &::before {
+        transform: rotate(240deg);
+        animation-delay: 6s;
+      }
+    }
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      inset: 25%;
+      width: 50%;
+      height: 50%;
+      border: 10px solid currentColor;
+      position: relative;
+      border-width: 10px 15px 10px 5px;
+      border-radius: 45% 40% 50% 40%;
+      animation: borderSize 2s ease-in-out infinite;
+      background-color: white;
+    }
+    &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      inset: 0px;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(3px);
+      border-radius: 50%;
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      border-width: 5px 10px 15px 10px;
+    }
+    100% {
+      transform: rotate(359deg);
+    }
+  }
+
+  @keyframes borderSize {
+    0% {
+      border-width: var(--border1) var(--border2) var(--border3) var(--border4);
+      opacity: var(--max-opacity);
+      scale: var(--min-scale);
+    }
+    25% {
+      border-width: var(--border4) var(--border1) var(--border2) var(--border3);
+    }
+    50% {
+      border-width: var(--border3) var(--border4) var(--border1) var(--border2);
+      opacity: var(--min-opacity);
+      scale: var(--max-scale);
+    }
+    75% {
+      border-width: var(--border2) var(--border3) var(--border4) var(--border1);
+    }
+    100% {
+      border-width: var(--border1) var(--border2) var(--border3) var(--border4);
+      opacity: var(--max-opacity);
+      scale: var(--min-scale);
+    }
+  }
+</style>
---
+++ src/lib/diary/2024-05-10-fuzzy-css-spinner-idea/page.md
@@ -0,0 +1,11 @@
+---
+title: Fuzzy CSS spinner idea
+date: 2024-05-10T23:23:16.207Z
+status: draft
+---
+
+<script lang="ts">
+  import CssSpinner from './CssSpinner.svelte';
+</script>
+
+<CssSpinner/>
